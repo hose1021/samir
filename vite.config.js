b05bbbe808ffcaca458a735fdfa7695da417b8e5
Hose <hose1021@gmail.com>
2025-01-18 19:40:12 +0400

    init

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,32 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-    base: '/samir/',
-    plugins: [
-        vue(),
-        vueDevTools()
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    build: {
-        assetsDir: 'assets',
-        rollupOptions: {
-            output: {
-                manualChunks: undefined
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        base: '/samir/',
+        plugins: [
+            vue(),
+            vueDevTools()
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
             }
+        },
+        build: {
+            assetsDir: 'assets',
+            rollupOptions: {
+                output: {
+                    manualChunks: undefined
+                }
+            }
+        },
+        define: {
+            'process.env': env
         }
     }
 })
